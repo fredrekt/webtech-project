f06cc864f7f9d5317483452ff0c7e903be6973ac
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-08-12 15:56:10 +0800

    added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Footer.css'
+const Footer = () =>{
+    return(
+      <div id="footer" className="container-fluid">
+              <p className="footer-txt">Copyright 2019 | Web Technologies Project</p>
+      </div>
+    )
+}
+export default Footer
