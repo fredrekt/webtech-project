204621b05570edf23a3230bd50f742d8b93d0e8a
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-08-12 15:57:24 +0800

    added the Components for renderDOm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import Menu from './components/Navbar'
 import Banner from './components/Carousel'
+import Content from './components/Jumbotron'
+import Features from './components/Features'
+import Footer from './components/Footer'
+import Contact from './components/Contact'
+import Clients from './components/Clients'
 
 function App() {
   return (
     <div className="App">
       <Menu/>
       <Banner/>
+      <Content/>
+      <Features/>
+      <Contact/>
+      <Clients />
+
+
+
+
+      <Footer/>
     </div>
   );
 }
