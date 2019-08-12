5617ccff34e941b022948bd91d79e21d98f151d9
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-08-12 23:06:46 +0800

    added sticky parameter

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import Nav from 'react-bootstrap/Nav'
 import './Navbar.css'
 const Menu = () =>{
     return(
-    <Navbar id="navbar-black">
+    <Navbar sticky="top" id="navbar-black">
         <Navbar.Brand id="brand-name" href="#home">shoe Shack</Navbar.Brand>
             <Nav id="white-txt" className="ml-auto">
                  <Nav.Link style={{color:"white", "text-transform":"uppercase","letter-spacing":'0.05em'}} className="mr-3" href="#home">Home</Nav.Link>
