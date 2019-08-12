80d5742df9c36d9e4b96b6c6d5149f8c9c88195e
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-08-12 16:15:52 +0800

    added indent for form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,27 +11,27 @@ const Contact = () =>{
         <Form>
             <Jumbotron style={{'background-color':'black', 'border-radius':'0px'}}>
                 <h1 className="header-txt">Get In Touch</h1>
-    <Form.Row>
+    <Form.Row style={{'margin-left':'2.7%'}}>
         <Form.Group as={Col} controlId="formGridEmail">
         <Form.Label className="form-label">Full Name </Form.Label>
         <Form.Control className="form-input" type="email" placeholder="Firstname                                              Middlename                                              Lastname" />
         </Form.Group>
     </Form.Row>
 
-    <Form.Group controlId="formGridAddress1">
+    <Form.Group style={{'margin-left':'3%'}} controlId="formGridAddress1">
         <Form.Label>Email</Form.Label>
         <Form.Control className="form-input" placeholder="example@mail.com" />
     </Form.Group>
 
-    <Form.Group controlId="formGridAddress2">
+    <Form.Group style={{'margin-left':'3%'}} controlId="formGridAddress2">
         <Form.Label>Address 2</Form.Label>
         <Form.Control className="form-input" placeholder="House No./ Building No.                           Street                                             Municipality" />
     </Form.Group>
-    <Form.Group controlId="exampleForm.ControlTextarea1">
+    <Form.Group style={{'margin-left':'3%'}} controlId="exampleForm.ControlTextarea1">
         <Form.Label>Message</Form.Label>
         <Form.Control className="form-input" as="textarea" rows="4" />
     </Form.Group>    
-    <Button size="lg" variant="dark" type="submit">
+    <Button style={{'margin-left':'3%'}} size="lg" variant="dark" type="submit">
         FIRE AWAY
     </Button>
     </Jumbotron>
