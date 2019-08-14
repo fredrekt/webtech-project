42d81473dfe3050e6925c0c442662175c9d7377c
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-08-15 01:51:50 +0800

    fixed text-transform property

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,11 @@ const Menu = () =>{
     <Navbar sticky="top" id="navbar-black">
         <Navbar.Brand id="brand-name" href="#home">shoe Shack</Navbar.Brand>
             <Nav id="white-txt" className="ml-auto">
-                 <Nav.Link style={{color:"white", "text-transform":"uppercase","letter-spacing":'0.05em'}} className="mr-3" href="#home">Home</Nav.Link>
-                    <Nav.Link style={{color:"white", "text-transform":"uppercase","letter-spacing":'0.05em'}} className="mr-3" href="#features">About</Nav.Link>
-                        <Nav.Link style={{color:"white", "text-transform":"uppercase","letter-spacing":'0.05em'}} href="#pricing">Shop</Nav.Link>
+                 <Nav.Link style={{color:"white", "text-transform":"","letter-spacing":'0.05em'}} className="mr-3" href="#home">Home</Nav.Link>
+                    <Nav.Link style={{color:"white", "text-transform":"","letter-spacing":'0.05em'}} className="mr-3" href="#features">About</Nav.Link>
+                        <Nav.Link style={{color:"white", "text-transform":"","letter-spacing":'0.05em'}} href="#pricing">Shop</Nav.Link>
         </Nav>
-    </Navbar>
+    </Navbar> 
         )
 }
 export default Menu
