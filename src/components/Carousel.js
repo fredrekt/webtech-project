e2710869eb6055bc70d927b20fe7cdda740184d0
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-08-15 01:50:25 +0800

    fixed carousel bug

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,7 +3,7 @@ import './Carousel.css'
 import Carousel from 'react-bootstrap/Carousel'
 import banner2 from '/Users/Acer/Desktop/webtech-project/src/img/nike-shoes.jpg'
 import finalbanner from '/Users/Acer/Desktop/webtech-project/src/img/banner1-final.png'
-import banner3 from '/Users/acer/Desktop/webtech-project/src/img/nike-shoes-3.jpg'
+import banner3 from '/Users/acer/Desktop/webtech-project/src/img/nikeshoes.png'
 const Banner = () =>{
     return(
         <Carousel>
@@ -13,7 +13,7 @@ const Banner = () =>{
       src={finalbanner}
       alt="First slide"
     />
-    <Carousel.Caption style={{"text-align":"right", "margin-bottom":"500px"}}>
+    <Carousel.Caption style={{'top':'0','bottom':'auto','left':'auto'}}>
       <h3 style={{"text-transform":"uppercase","font-size":"60px","font-style":"bold","font-family":'Changa, sans-serif'}}>shoe shack</h3>
       <p style={{"font-size":"20px", "margin-top":"-20px"}}>Choose Shoes. Buy Shoes. Get Shoes.</p>
     </Carousel.Caption>
@@ -21,7 +21,7 @@ const Banner = () =>{
   <Carousel.Item className="carousel-height">
     <img
       className="d-block w-100"
-      src={banner2}
+      src={banner3}
       alt="Third slide"
     />
 
@@ -32,8 +32,9 @@ const Banner = () =>{
   </Carousel.Item>
   <Carousel.Item className="carousel-height">
     <img
+      
       className="d-block w-100"
-      src={banner3}
+      src={banner2}
       alt="Third slide"
     />
     <Carousel.Caption className="align-top-left">
