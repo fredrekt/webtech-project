54f2e4cc0f1c47a9f462fb14cff9fed173109010
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-08-29 17:47:41 +0800

    added BrowserRouter and switch for the react-router-dom

--- src/App.js
+++ src/App.js
@@ -11,12 +11,23 @@ import Shop from './components/Shop'
 import About from './components/About'
 import Team from './components/Team'
 import HoursRendered from './components/HoursRendered'
+import AboutComponent from './components/AboutComponent'
+import HomeComponent from './components/HomeComponent'
+
+import {BrowserRouter, Switch ,Route} from 'react-router-dom'
 
 
 function App() {
   return (
+    <BrowserRouter>
+    
     <div className="App">
       <Menu/>
+      <Switch>
+        <Route path='/'exact component={HomeComponent} />
+        <Route path='/about-us/' component={AboutComponent}/>
+      </Switch>
+     
        {/* About Section Starts here
       Add to browser router - /About */}
 
@@ -28,12 +39,12 @@ function App() {
 
       {/* About Section Ends here */}
      
-      
+{/*       
       <Banner/>
       <Content/>
       <Features/>
       <Contact/>
-      <Clients />
+      <Clients /> */}
 
       {/*
         commented 50% done - add containers for the items (shoes)
@@ -46,6 +57,7 @@ function App() {
 
       <Footer/>
     </div>
+    </BrowserRouter>
   );
 }
 
