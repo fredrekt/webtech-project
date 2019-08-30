8fcfc30bc5048aad6f1ee9eea24ffec6276b9635
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-08-30 17:00:31 +0800

    added animations for Carousel Headers

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,6 +4,49 @@ import Carousel from 'react-bootstrap/Carousel'
 import banner2 from '../img/nike-shoes.jpg'
 import finalbanner from '../img/banner1-final.png'
 import banner3 from '../img/nikeshoes.png'
+import Button from 'react-bootstrap/Button'
+import styled, { keyframes } from 'styled-components'
+import { bounce } from 'react-animations'
+import { pulse } from 'react-animations'
+import { flash } from 'react-animations'
+import { bounceInLeft} from 'react-animations'
+import { bounceInDown } from 'react-animations'
+
+const bounceAnimation = keyframes`${bounce}`
+const pulseAnimation = keyframes`${pulse}`
+const flashAnimation = keyframes`${flash}`
+const bounceLeftAnimation = keyframes`${bounceInLeft}`
+const bounceInDownAnimation = keyframes`${bounceInDown}`
+
+const BouncyDiv = styled.div`
+  animation: 1s ${bounceAnimation}
+`;
+const BouncyDiv2 = styled.div`
+  animation: 4s ${pulseAnimation}
+`;
+const FlashyDiv = styled.div`
+  animation: 1s ${flashAnimation}
+  `;
+const BouncyLeftDiv = styled.div`
+  animation: 3s ${bounceLeftAnimation}
+  `;
+const BouncyLeftDiv2 = styled.div`
+  animation: 4s ${bounceLeftAnimation}
+  `;
+const BouncyLeftDiv3 = styled.div`
+  animation: 5s ${bounceLeftAnimation}
+  `;
+const BouncyDown = styled.div`
+  animation: 2s ${bounceInDownAnimation}
+  `;
+const BouncyDown2 = styled.div`
+  animation: 4s ${bounceInDownAnimation}
+  `;
+
+
+
+
+
 const Banner = () =>{
     return(
         <Carousel>
@@ -13,10 +56,11 @@ const Banner = () =>{
       src={finalbanner}
       alt="First slide"
     />
-    <Carousel.Caption style={{'top':'0','bottom':'auto','left':'auto'}}>
-      <h3 style={{"text-transform":"uppercase","font-size":"60px","font-style":"bold","font-family":'Changa, sans-serif'}}>shoe shack</h3>
-      <p style={{"font-size":"20px", "margin-top":"-20px"}}>Choose Shoes. Buy Shoes. Get Shoes.</p>
-      <p style={{"font-size":"15px", "margin-top":"-20px"}}>The Nike Jones Twice Apex Legends Underlord</p>
+    <Carousel.Caption style={{'top':'0','bottom':'auto','left':'auto','margin-top':'5%'}}>
+      <BouncyDiv><h3 style={{"text-transform":"uppercase","font-size":"60px","font-style":"bold","font-family":'Changa, sans-serif'}}>shoe shack</h3></BouncyDiv>
+      <BouncyDiv2><p style={{"font-size":"20px", "margin-top":"-20px"}}>Choose Shoes. Buy Shoes. Get Shoes.</p></BouncyDiv2>
+      <FlashyDiv><p style={{"font-size":"15px", "margin-top":"-20px"}}>The Nike Jones Twice Apex Legends Underlord</p></FlashyDiv>
+      {/* <Button size="lg"variant="outline-secondary">GET ON</Button> */}
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item className="carousel-height">
@@ -27,9 +71,9 @@ const Banner = () =>{
       alt="Third slide"
     />
     <Carousel.Caption className="align-top-left">
-      <h3 style={{'text-transform':'uppercase', 'font-family':'Changa, sans-serif','font-size':'40px'}}>Third slide label</h3>
-      <p style={{'font-size':'20px','margin-top':'-15px'}}>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      <p style={{'font-size':'15px','margin-top':'-17px'}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eu purus nec <br/>risus consectetur tempus vel sed quam. Cras pulvinar leo ac lacinia ultrices. Mauris <br/>vel metus at dui condimentum gravida ut in nisi. VivaVivamus pulvinar accumsan hendrerit.</p>
+      <BouncyLeftDiv><h3 style={{'text-transform':'uppercase', 'font-family':'Changa, sans-serif','font-size':'40px'}}>Nike Ultraboost <br/>Hyperspace</h3></BouncyLeftDiv>
+      <BouncyLeftDiv2><p style={{'font-size':'20px','margin-top':'-15px'}}>Wear and Tear by Kobe Bryant Paras</p></BouncyLeftDiv2>
+      <BouncyLeftDiv3><p style={{'font-size':'15px','margin-top':'-17px'}}>This shoes is used for Training, Running and Body Building. Experts say <br/>that using this shoes boosts' performance as a person or an Athlete.</p></BouncyLeftDiv3>
     </Carousel.Caption>
   </Carousel.Item>
   <Carousel.Item className="carousel-height">
@@ -38,11 +82,12 @@ const Banner = () =>{
       src={banner3}
       alt="Third slide"
     />
-
-    <Carousel.Caption>
-      <h3 style={{"font-family":"Changa, sans-serif","text-transform": "uppercase","font-size":"40px"}}>Nike Kaishi Sports</h3>
-      <p style={{"font-size":"20px","margin-top":"-20px"}}>Made by Athletes, for athletes</p>
+  
+    <Carousel.Caption style={{"top":'0','right':'auto','margin-top':'10%'}}>
+      <BouncyDown><h3 style={{"font-family":"Changa, sans-serif","text-transform": "uppercase","font-size":"40px"}}>Nike Kaishi Sports</h3></BouncyDown>
+      <BouncyDown2><p style={{"font-size":"20px","margin-top":"-20px"}}>Made by Athletes, for athletes</p></BouncyDown2>
     </Carousel.Caption>
+  
   </Carousel.Item>
   
 </Carousel>
