5c48ee7e1d2620afba0b2824fb83a7493cac3b1e
Fredrick Garingo <fredrickjohng6@gmail.com>
2019-08-30 17:54:48 +0800

    added scrollAnimation for Contact Form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -7,6 +7,11 @@ import Container from 'react-bootstrap/Container'
 import './Contact.css'
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import mime from '../img/mime.jpg'
+
+import ScrollAnimation from 'react-animate-on-scroll';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+import Popover from 'react-bootstrap/Popover'
+
 const Contact = () =>{
     return(
     <Container fluid>
@@ -15,14 +20,33 @@ const Contact = () =>{
                 <Form>
                             
                             <Jumbotron style={{'background-color':'black', 'border-radius':'0px'}}>
-                                <h1 className="header-txt">Get In Touch</h1>
+                                <ScrollAnimation animateIn='bounceInLeft'
+                                animateOut='bounceOutRight'> 
+                                <OverlayTrigger
+                    key='top'
+                    placement='right'
+                    overlay={
+                    <Popover id={`popover-positioned-$placement`}>
+                                        <Popover.Title as="h3"></Popover.Title>
+                                        <Popover.Content>
+                                            <strong>This is how you reach us</strong>
+                                        </Popover.Content>
+                                        </Popover>
+                    }
+                >
+                                    <h1 className="header-txt">Get In Touch</h1>
+                               </OverlayTrigger> 
+                                </ScrollAnimation>
+                    <ScrollAnimation animateIn='bounceInUp'
+                    animateOut='bounceOutRight'>             
                     <Form.Row style={{'margin-left':'2.7%'}}>
                         <Form.Group as={Col} controlId="formGridEmail">
                         <Form.Label className="form-label">Full Name </Form.Label>
                         <Form.Control className="form-input" type="email" placeholder="Firstname                                              Middlename                                              Lastname" />
                         </Form.Group>
                     </Form.Row>
-
+                    
+                
                     <Form.Group style={{'margin-left':'3%'}} controlId="formGridAddress1">
                         <Form.Label>Email</Form.Label>
                         <Form.Control className="form-input" placeholder="example@mail.com" />
@@ -35,10 +59,13 @@ const Contact = () =>{
                     <Form.Group style={{'margin-left':'3%'}} controlId="exampleForm.ControlTextarea1">
                         <Form.Label>Message</Form.Label>
                         <Form.Control className="form-input" as="textarea" rows="4" />
-                    </Form.Group>    
+                    </Form.Group> 
+                    
                     <Button style={{'margin-left':'3%'}} size="lg" variant="dark" type="submit">
                         FIRE AWAY
                     </Button>
+                    </ScrollAnimation>
+
                     </Jumbotron>
                 </Form>
             </Col>
